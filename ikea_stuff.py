76cac7fc6f3a963ef0bd64206011abd25645b950
Noah Gardner <noahercow@gmail.com>
2018-06-22 10:27:10 -0500

    Revert "Initial commit"
    This reverts commit 62a60b6adb9ce0ce3faf850625d6a3128483b7f4.

--- ikea_stuff.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-page_link ='https://www.ikea.com/us/en/catalog/categories/departments/living_room/39130/'
-page_response = requests.get(page_link, timeout=5)
-page_content = BeautifulSoup(page_response.content, "html.parser")
-fullTitle = page_content.find_all(class_="productTitle floatLeft")
-print fullTitle
